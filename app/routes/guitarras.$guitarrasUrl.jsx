89d34229f5eddcc9de42e21b33d61cfb65a68b41
Abel <90abel.avr@gmail.com>
2024-02-17 01:57:22 -0300

    added route each guitar and components

--- app/routes/guitarras.$guitarrasUrl.jsx
+++ app/routes/guitarras.$guitarrasUrl.jsx
@@ -1,11 +1,49 @@
+import { useLoaderData } from "@remix-run/react"
+import { getGuitarra } from '~/models/guitarras.server'
+import styles from "~/styles/guitarras.css"
+
+export function meta({data}){
+// Una vez que el loader tiene informacion, se pasa automaticamente y data se hace disponible
+return(
+  [
+      {
+          title:`GuitarLA - ${data.data[0].attributes.nombre}`
+      },
+      {
+          description:`Venta de guitarras, nuestra coleccion, guitarra ${data.data[0].attributes.nombre}`      
+      }
+  ]
+)
+}
+
 export async function loader({params}){
-    // console.log(request);
-    console.log(params);
-    return {}
+    const { guitarrasUrl } = params
+    const guitarra = await getGuitarra(guitarrasUrl)
+    // console.log(info);
+    return guitarra
+}
+export function links(){
+  return[
+    {
+      rel:"stylesheet",
+      href:styles
+    }
+  ]
 }
 const GuitarraUrl = () => {
+    const guitarra = useLoaderData()
+    const {nombre, descripcion, imagen, precio} = guitarra.data[0].attributes
+    // console.log(guitarra.data[0].attributes); 
+    // console.log();
   return (
-    <div>$guitarraUrl</div>
+    <main className="contenedor guitarra">
+      <img src={imagen.data.attributes.formats.medium.url} alt={`imagen guitarra ${nombre}`} />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion[0].children[0].text}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
   )
 }
 
